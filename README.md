58e0da1fbc967628e9960457ba7839b3dc74ac14
OnlineAlex <becasualua@gmail.com>
2018-04-07 22:13:54 +0300

    рабочая версия скрипта / дополнил README

--- README.md
+++ README.md
@@ -6,20 +6,17 @@ This project helps to control time was spent on sites. It requires special exten
 
 Install extension for Chrome browser [Custom JavaScript for websites](https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija).
 
-Open configuration of [cjs](https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija) browser extension on the site you want to controll. Click on the link "your own external scripts", add path [TODO в качестве production хостинга файла с JavaScript подойдет GitHub - у каждого файла в репозитории есть свой URL, и этим можно воспользоваться]. Don`t forget to press "enable cjs for this host" to enable custom JS.
-
-[TODO describe what will happen after that]
+Open configuration of [cjs](https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija) browser extension on the site you want to controll. Click on the link "your own external scripts", add path [https://cdn.rawgit.com/OnlineAlex/34_timemachine/0250a2fa/index.js](https://cdn.rawgit.com/OnlineAlex/34_timemachine/0250a2fa/index.js). Don`t forget to press "enable cjs for this host" to enable custom JS. Press "save".
+The page will reload and the left upper corner will appear a timer.
 
 For faster development you can use JS code hosted on localhost. Simple web server can be used for that, run:
 
 ```bash
 
-python3 -m http.server
+python -m http.server #or python3
 ```
 
 Add path `http://localhost:8000/index.js` to [cjs](https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija) browser extension. Done.
-
-
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var TIMEOUT_IN_SECS = 3 * 60
-var TEMPLATE = '<h1><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>'
+var TIMEOUT_IN_SECS =  3 * 60
+var TEMPLATE = '<h1 style="margin: 0"><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>'
+var MOTIVATIONAL_QUOTES = [
+  'Не ждите. Подходящее время никогда не наступит.\n~ Наполеон Хилл',
+  'Унция действия стоит тонны теории.\n~ Фридрих Энгельс',
+  'Откладывайте на завтра лишь то, что вы не хотите завершить до самой смерти.\n~ Пабло Пикассо',
+  'Прокрастинация — вор времени. Держите ее в узде.\n~ Чарльз Диккенс',
+  'Не прокрастинируйте — чем дольше вы оставляете важное на потом, тем больше оно покажется, когда на вас обрушится час «X».\n ~ Аурора МакКлеллан',
+  'Откладывание легких вещей делает их трудными. Откладывание трудных вещей делает их невозможными.\n~ Джордж Х. Лоример',
+  'Вы не сможете избежать завтрашней ответственности, уконяясь от нее сегодня.\n~ Авраам Линкольн',
+  'Начните. Делайте что-нибудь… Движение — лучшее лекарство от прокрастинации.\n~ Энтони Геррера',
+  'Через год ты будешь думать о том, что лучше бы ты начал сегодня.\n~ Карен Лэмб',
+  'Еще не все колеса изобретены: мир слишком удивителен, чтобы сидеть сложа руки.\n~ Ричард Брэнсон',
+  'Когда люди заняты работой, у них бывает лучшее настроение.\n~ Бенджамин Франклин'
+]
 
 function padZero(number){
   return ("00" + String(number)).slice(-2);
@@ -10,6 +23,7 @@ class Timer{
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
   constructor(timeout_in_secs){
     this.initial_timeout_in_secs = timeout_in_secs
+
     this.reset()
   }
   getTimestampInSecs(){
@@ -34,6 +48,13 @@ class Timer{
     this.timestampOnStart = null
     this.timeout_in_secs = this.initial_timeout_in_secs
   }
+  restart(second){
+    this.timeout_in_secs = second
+    this.timestampOnStart = this.getTimestampInSecs()
+
+    this.isRunning = true
+
+  }
   calculateSecsLeft(){
     if (!this.isRunning)
       return this.timeout_in_secs
@@ -56,7 +77,20 @@ class TimerWidget{
     // adds HTML tag to current page
     this.timerContainer = document.createElement('div')
 
-    this.timerContainer.setAttribute("style", "height: 100px;")
+    this.timerContainer.setAttribute("style",
+      "font-size: 15px;" +
+      "padding: 26px 5px;" +
+      "color: #ff6666;" +
+      "top: 10px;" +
+      "left: 10px;" +
+      "position: fixed;" +
+      "z-index: 101;" +
+      "background-color: #f6f6f6;" +
+      "border-radius: 50%;" +
+      "border: 5px dashed #505050;"
+    )
+
+
     this.timerContainer.innerHTML = TEMPLATE
 
     rootTag.insertBefore(this.timerContainer, rootTag.firstChild)
@@ -85,13 +119,33 @@ function main(){
   var timer = new Timer(TIMEOUT_IN_SECS)
   var timerWiget = new TimerWidget()
   var intervalId = null
-
+  var timeoutAlert = 30
+  var notice = MOTIVATIONAL_QUOTES.slice()
   timerWiget.mount(document.body)
+
+
+  function noticeAlert() {
+    var notification = Math.floor(Math.random() * notice.length)
+
+    window.alert(notice[notification])
+    notice.splice(notification, 1)
+
+    if (notice.length < 1){
+      notice = MOTIVATIONAL_QUOTES.slice()
+    }
+  }
+
 
   function handleIntervalTick(){
     var secsLeft = timer.calculateSecsLeft()
     timerWiget.update(secsLeft)
+
+    if (secsLeft === 0) {
+      noticeAlert()
+      timer.restart(timeoutAlert)
+    }
   }
+
 
   function handleVisibilityChange(){
     if (document.hidden) {
@@ -108,6 +162,7 @@ function main(){
   document.addEventListener("visibilitychange", handleVisibilityChange, false);
   handleVisibilityChange()
 }
+
 
 // initialize timer when page ready for presentation
 window.addEventListener('load', main)
